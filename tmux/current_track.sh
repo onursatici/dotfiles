22c530b3579d7514b5fbe384594253d6b23b6e21
Onur Satici <onursatici@gmail.com>
2016-01-03 17:22:01 +0200

    fix current track script

--- tmux/current_track.sh
+++ tmux/current_track.sh
@@ -1,32 +1,17 @@
 #!/bin/zsh
 
 NOW_PLAYING=$(osascript <<EOF
-  set spotify_state to false
-  set itunes_state to false
-  if is_app_running("Spotify") then
-    tell application "Spotify" to set spotify_state to (player state as text)
+  if application "Spotify" is running then
+      tell application "Spotify"
+        set theName to name of the current track
+        set theArtist to artist of the current track
+        set theAlbum to album of the current track
+        set theUrl to spotify url of the current track
+        try
+          return theName & " - " & theArtist
+        on error err
+        end try
+      end tell
   end if
-  if is_app_running("iTunes") then
-    tell application "iTunes" to set itunes_state to (player state as text)
-  end if
-  (* Whatever other music applications you use *)
-  if spotify_state is equal to "playing" then
-    tell application "Spotify"
-      set track_name to name of current track
-      set artist_name to artist of current track
-      return track_name & " - " & artist_name
-    end tell
-  else if itunes_state is equal to "playing" then
-    tell application "iTunes"
-      set track_name to name of current track
-      set artist_name to artist of current track
-      return track_name & " - " & artist_name
-  end tell
-  else
-  return "-"
-  end if
-  on is_app_running(app_name)
-    tell application "System Events" to (name of processes) contains app_name
-  end is_app_running
-  EOF)
+EOF)
 echo $NOW_PLAYING
